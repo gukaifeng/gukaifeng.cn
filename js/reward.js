c94b92069841c114d559339483d82c06d4353db3
gukaifeng <892859816@qq.com>
2023-06-21 17:10:16 +0800

    Add QR code

--- js/reward.js
+++ js/reward.js
@@ -15,7 +15,7 @@ function reward(){
         Swal.fire({
           title: '感谢您',
           html: '请打开支付宝 <b>[扫一扫]</b> 以充电',
-          imageUrl: '/media/alipay.webp',
+          imageUrl: 'https://bu.dusays.com/2023/06/21/6492bc949db84.jpg',
           imageWidth: 175,
           imageHeight: 175,
           imageAlt: 'Custom image'
@@ -32,7 +32,7 @@ function reward(){
         Swal.fire({
           title: '感谢您',
           html: '请打开微信 <b>[扫一扫]</b> 以充电',
-          imageUrl: '/media/wechat.webp',
+          imageUrl: 'https://bu.dusays.com/2023/06/21/6492bc93b1311.jpg',
           imageWidth: 175,
           imageHeight: 175,
           imageAlt: 'Custom image'
